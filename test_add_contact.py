ee7ca7b5b829b8e2d447d88a6606b2731581a28e
Alex Donskov <mrad@list.ru>
2018-08-26 11:55:51 +0300

    Выделил параметры в методах Login и Creat_contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -17,34 +17,34 @@ class test_add_contact(unittest.TestCase):
     def Open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def Login(self, wd):
+    def Login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def Create_contact(self, wd):
+    def Create_contact(self, wd, firtname, lastname, address, home, email):
         # open contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("qwe")
+        wd.find_element_by_name("firstname").send_keys(firtname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("qwe")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("qwe")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("qwe")
+        wd.find_element_by_name("home").send_keys(home)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("qwe")
+        wd.find_element_by_name("email").send_keys(email)
         # Enter
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -58,8 +58,8 @@ class test_add_contact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.Open_home_page(wd)
-        self.Login(wd)
-        self.Create_contact(wd)
+        self.Login(wd, username="admin", password="secret")
+        self.Create_contact(wd, firtname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe")
         self.Return_contact_list(wd)
         self.Logout(wd)
 
