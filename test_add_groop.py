6b8000973ef9d4ed26da1a62b45dbf778b7013ab
Alex Donskov <mrad@list.ru>
2018-08-26 20:53:57 +0300

    Сделал фикстуру. Все вспомогательные методы теста test_add_groop перенесены в класс Application. Переделан запуск тестов unittest с на py.test

--- test_add_groop.py
+++ test_add_groop.py
@@ -1,69 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_groop(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_groops_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def create_groop(self, wd, group):
-        self.open_groops_page(wd)
-        # init groop creation
-        wd.find_element_by_name("new").click()
-        # fill groop firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit groop creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groops_page(wd)
-
-    def return_to_groops_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def Logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def test_add_groop(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_groop(wd, Group(name="qweqwe", header="qweqwe", footer="qweqwe"))
-        self.Logout(wd)
+def test_add_groop(app):
+    app.login(username="admin", password="secret")
+    app.create_groop(Group(name="qweqwe", header="qweqwe", footer="qweqwe"))
+    app.Logout()
 
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
