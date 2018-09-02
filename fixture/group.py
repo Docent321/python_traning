1315748b4285bcd2e4973c717db462098c517972
Alex Donskov <mrad@list.ru>
2018-09-02 15:17:02 +0300

    Выделил методы "Выбор первой группы" select_first_group и "Заполнение полей группы" fill_group_firm

--- fixture/group.py
+++ fixture/group.py
@@ -14,38 +14,13 @@ class GroupHelper:
         self.open_groops_page()
         # init groop creation
         wd.find_element_by_name("new").click()
-        # fill groop firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_firm(group)
         # submit groop creation
         wd.find_element_by_name("submit").click()
         self.return_to_groops_page()
 
-    def delete_first_group(self):
+    def fill_group_firm(self, group):
         wd = self.app.wd
-        self.open_groops_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
-        #submit deletion
-        wd.find_element_by_name("delete").click()
-        self.return_to_groops_page()
-
-    def change_first_group(self, group):
-        wd = self.app.wd
-        self.open_groops_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # submit edit group
-        wd.find_element_by_name("edit").click()
-        # clear field group
-        # fill groop firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -55,6 +30,26 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groops_page()
+        self.select_first_group()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groops_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def change_first_group(self, group):
+        wd = self.app.wd
+        self.open_groops_page()
+        self.select_first_group()
+        # submit edit group
+        wd.find_element_by_name("edit").click()
+        self.fill_group_firm(group)
         # submit update
         wd.find_element_by_name("update").click()
         self.return_to_groops_page()
