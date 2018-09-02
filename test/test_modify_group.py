061375dfacb91536b65919602dc719b0f283ecb1
Alex Donskov <mrad@list.ru>
2018-09-02 17:03:20 +0300

    Сделаны тесты для модификации имени, хедера и футера группы по отдельности

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,3 +5,18 @@ def test_modify_first_groop(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="1", header="1", footer="1"))
     app.session.logout()
+
+def test_modify_groop_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_groop_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()
+
+def test_modify_groop_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="New footer"))
+    app.session.logout()
