0bbe924274bc92321f1044435e891f5f7baec4b9
Alex Donskov <mrad@list.ru>
2018-09-02 21:14:03 +0300

    Доработал тесты модификации и удаления контакта, чтобы при отсутсвии в списке хотя бы 1 контакта, тесты сами его создавали.

--- fixture/contact.py
+++ fixture/contact.py
@@ -51,5 +51,8 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_id("logo").click()
 
+    def count(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_name("selected[]"))
 
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,19 +3,31 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
+    if app.contact.count() == 0:
+         app.contact.create(Contact(firstname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe"))
     app.contact.modify_first_contact(Contact(firstname="1", lastname="1", address="1", home="1", email="1"))
 
 def test_modify_contact_firtname(app):
+    if app.contact.count() == 0:
+         app.contact.create(Contact(firstname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe"))
     app.contact.modify_first_contact(Contact(firstname="firtname"))
 
 def test_modify_contact_lastname(app):
+    if app.contact.count() == 0:
+         app.contact.create(Contact(firstname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe"))
     app.contact.modify_first_contact(Contact(lastname="New lastname"))
 
 def test_modify_contact_address(app):
+    if app.contact.count() == 0:
+         app.contact.create(Contact(firstname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe"))
     app.contact.modify_first_contact(Contact(address="New address"))
 
 def test_modify_contact_home(app):
+    if app.contact.count() == 0:
+         app.contact.create(Contact(firstname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe"))
     app.contact.modify_first_contact(Contact(home="New home"))
 
 def test_modify_contact_email(app):
+    if app.contact.count() == 0:
+         app.contact.create(Contact(firstname="qwe", lastname="qwe", address="qwe", home="qwe", email="qwe"))
     app.contact.modify_first_contact(Contact(email="New email"))
