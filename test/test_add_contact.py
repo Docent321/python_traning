386b35710ea606cbd479fa29b0026f25f72a33e4
Alex Donskov <mrad@list.ru>
2018-09-16 14:34:17 +0300

    Параметризовал тест добавления контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", lastname="", address="", homephone="", email="", mobilephone="",workphone="", secondaryphone="")] + \
+           [Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10),
+            address=random_string("address", 10), homephone=random_string("homephone", 10),
+            email=random_string("email", 10), mobilephone=random_string("mobilephone", 10),
+            workphone=random_string("workphone", 10), secondaryphone=random_string("secondaryphone", 10))
+    for i in range(5)
+    ]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="qwe", lastname="qwe", address="qwe", homephone="12", email="qwe", mobilephone="23",
-                      workphone="34", secondaryphone="45")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
