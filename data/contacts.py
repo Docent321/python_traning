eb4b9f6ba9821f6e32dce954c83754d40b8f66c5
Alex Donskov <mrad@list.ru>
2018-09-23 19:26:37 +0300

    Сделал генератор данных и реализовал загрузку тестовых данных из файлов в формате JSON для контактов используя бибиотеку jsonpickle

--- data/contacts.py
+++ data/contacts.py
@@ -1,7 +1,7 @@
 from model.contact import Contact
 
 
-constant = [
+testdata = [
     Contact(firstname="firstname1", lastname="lastname1", address="address1", homephone="homephone1", email="email1",
             email2="email21", email3="email31", mobilephone="mobilephone1",workphone="workphone1", secondaryphone="secondaryphone1"),
     Contact(firstname="firstname2", lastname="lastname2", address="address2", homephone="homephone2", email="email2",
